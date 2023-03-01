4030aa627803c1f3951c84982d2275e9d0673155
emilioeiji <emilioeiji@gmail.com>
2023-03-01 10:44:34 +0900

    Implementação da tela de login.

--- contas/urls.py
+++ contas/urls.py
@@ -5,4 +5,7 @@ from . import views
 
 urlpatterns = [
     path('perfil/', views.perfil, name='perfil'),
+    path('login/', views.processa_login, name='login'),
+    path('logout/', views.processa_logout, name='logout'),
+    path('redirect_home/', views.processa_redirect_home, name='go_home')
 ]
