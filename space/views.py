271aa59e532159ee53895a5d7f9bf6722b585626
emilioeiji <emilioeiji@gmail.com>
2023-07-14 23:53:21 +0900

    Implementado botão envia mensagem

--- space/views.py
+++ space/views.py
@@ -1,9 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseNotAllowed
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 
-from .models import Space, Tema, Topico
+from .models import Mensagem, Space, Tema, Topico
 
 
 @login_required()
@@ -31,7 +31,15 @@ def lista_space(request, space_id):
 @login_required()
 def lista_tema(request, tema_id):
     topico = get_object_or_404(Topico, id=tema_id)
+    space_id = topico.tema.space_id
     mensagens = topico.mensagem_set.order_by('-data_hora_criacao')
+
+    if request.method == 'POST':
+        mensagem = request.POST.get('mensagem')
+        nova_mensagem = Mensagem(
+            topico=topico, usuario=request.user, mensagem=mensagem)
+        nova_mensagem.save()
+        return redirect('lista_space', space_id=space_id)
 
     context = {
         'topico': topico,
