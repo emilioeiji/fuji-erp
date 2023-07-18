dbb4f46ebd4cce351bc0f6b309aa3f3494df5315
emilioeiji <emilioeiji@gmail.com>
2023-07-18 11:00:05 +0900

    Implementando visualização de mensagens

--- space/urls.py
+++ space/urls.py
@@ -7,5 +7,7 @@ from . import views
 urlpatterns = [
     path('', views.space, name='space'),
     path('lista-space/<int:space_id>/', views.lista_space, name='lista_space'),
+    path('lista-mensagens/<int:space_id>/',
+         views.lista_mensagens, name='lista_mensagens'),
     path('lista-tema/<int:tema_id>/', views.lista_tema, name='lista_tema'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- space/views.py
+++ space/views.py
@@ -38,12 +38,16 @@ def lista_tema(request, tema_id):
     space_id = topico.tema.space_id
     mensagens = topico.mensagem_set.order_by('-data_hora_criacao')
 
+    for mensagem in mensagens:
+        mensagem.lida = mensagem.leituramensagem_set.filter(
+            usuario=request.user, lida=True).exists()
+
     if request.method == 'POST':
         mensagem = request.POST.get('mensagem')
         nova_mensagem = Mensagem(
             topico=topico, usuario=request.user, mensagem=mensagem)
         nova_mensagem.save()
-        return redirect('lista_space', space_id=space_id)
+        return redirect('lista_mensagens', space_id=space_id)
 
     context = {
         'topico': topico,
@@ -51,3 +55,14 @@ def lista_tema(request, tema_id):
     }
 
     return render(request, 'space/lista_tema.html', context)
+
+
+@login_required()
+def lista_mensagens(request, space_id):
+    temas = Tema.objects.filter(space_id=space_id).order_by('nome')
+
+    context = {
+        'temas': temas,
+    }
+
+    return render(request, 'space/lista_mensagens.html', context)
