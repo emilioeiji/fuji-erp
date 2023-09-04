8844163f221b5fad80d1030c3131321487a347e3
Emilio Eiji <emilioeiji@gmail.com>
2023-09-04 10:39:40 +0900

    Static root

--- core/settings.py
+++ core/settings.py
@@ -140,7 +140,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL)
+# STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = 'img/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/img')
